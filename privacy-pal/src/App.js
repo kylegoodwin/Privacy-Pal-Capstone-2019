35ca2835e0bf45feba6b8ba72d3b6c9be928f8ab
RGPeart <rgpeart@hotmail.com>
2019-03-01 13:16:50 -0800

    added partial router

--- privacy-pal/src/App.js
+++ privacy-pal/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Redirect, NavLink} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.scss';
 import Header from './Header';
@@ -10,10 +11,46 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <Header></Header>
-      <MainBody></MainBody>
-      {/* <LearningPage></LearningPage> */}
+        
+      <NavBar />
+
+      <main>
+        <BrowserRouter>
+          <Route path="/home-page" component={MainBody}/>
+          {/* <MainBody></MainBody> */}
+          <Route path="/learning-page" component={LearningPage}/>
+          {/* <LearningPage></LearningPage> */}
+          <Redirect to="/home-page"/>
+        </BrowserRouter>
+      </main>
+
       <Footer></Footer>
+      </div>
+    );
+  }
+}
+
+class NavBar extends Component {
+  render() {
+    return (
+      <div className="navbar fixed-top">
+        <a>
+          {/* <Header></Header> */}
+          <img src="img/HeaderLogo.png" className="logo"></img>
+        </a>
+        <div className="navLinks">
+          <ul>
+            <li className="nav-item">
+              <NavLink to="/home-page" className="nav-link" activeClassName="selected" activeStyle={{fontWeight: "bold", color:"green"}}>Home</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/learning-page" className="nav-link" activeClassName="selected" activeStyle={{fontWeight: "bold", color:"green"}}>Learn</NavLink>
+            </li>
+            {/* <li className="nav-item">
+              <NavLink to="/" className="nav-link" activeClassName="selected" activeStyle={{fontWeight: "bold", color:"green"}}>About</NavLink>
+            </li> */}
+          </ul>
+        </div>
       </div>
     );
   }
