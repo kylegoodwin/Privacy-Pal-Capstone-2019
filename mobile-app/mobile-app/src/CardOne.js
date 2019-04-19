aff83be8810b8bd69b9fd9f5cf9a68733efcdee2
Kyle Goodwin <kjgoodwins@gmail.com>
2019-04-18 23:24:28 -0700

    Added baseline story functionality

--- mobile-app/mobile-app/src/CardOne.js
+++ mobile-app/mobile-app/src/CardOne.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
-import { NavBar } from './NavBar';
 import { Link } from 'react-router-dom';
+import {SeeMore} from './SeeMore';
+import Stories from 'react-insta-stories';
 
 export class CardOne extends Component {
+
+  
     render() {
+        let stories = [{ url: 'https://picsum.photos/1080/1920', seeMore: <SeeMore />, header: { heading: 'Mohit Karekar', subheading: 'Posted 5h ago', profileImage: 'https://picsum.photos/1000/1000' } }, { url: 'https://fsa.zobj.net/crop.php?r=dyJ08vhfPsUL3UkJ2aFaLo1LK5lhjA_5o6qEmWe7CW6P4bdk5Se2tYqxc8M3tcgYCwKp0IAyf0cmw9yCmOviFYb5JteeZgYClrug_bvSGgQxKGEUjH9H3s7PS9fQa3rpK3DN3nx-qA-mf6XN', header: { heading: 'Mohit Karekar', subheading: 'Posted 32m ago', profileImage: 'https://picsum.photos/1080/1920' } }, { url: 'https://media.idownloadblog.com/wp-content/uploads/2016/04/iPhone-wallpaper-abstract-portrait-stars-macinmac.jpg', header: { heading: 'mohitk05/react-insta-stories', subheading: 'Posted 32m ago', profileImage: 'https://avatars0.githubusercontent.com/u/24852829?s=400&v=4' } }, { url: 'https://storage.googleapis.com/coverr-main/mp4/Footboys.mp4', type: 'video', duration: 1000 }, { url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4', type: 'video', seeMore: <SeeMore /> }, { url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4', type: 'video' }, { url: 'https://picsum.photos/1080/1920', seeMore: <SeeMore />, header: { heading: 'Mohit Karekar', subheading: 'Posted 5h ago', profileImage: 'https://picsum.photos/1000/1000' },isLastStory: true }]
         return (
-        <div>
-            <Link to='/' className="previous">&#8249;</Link>
-            <NavBar></NavBar>
-        </div>
-     
+            <Stories 
+            stories={stories}
+            defaultInterval={1500}
+            width={window.innerWidth}
+            height={window.innerHeight}
+          />
         )
     }
 }
