4a2c911e5a4be695b4bf8b14a72d76fa87ea2cd7
Kyle Goodwin <kjgoodwins@gmail.com>
2019-04-30 16:42:12 -0700

    Updates

---
+++ precache-manifest.c9f8e46c16ff6dffaec9942565d27aab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "www.privacypal.me/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c230059d3499afd04add02c0769178d2",
+    "url": "www.privacypal.me/static/media/landing2.c230059d.jpg"
+  },
+  {
+    "revision": "8839c94819d9068dd60ee7a1c739b29d",
+    "url": "www.privacypal.me/static/media/landguy.8839c948.jpg"
+  },
+  {
+    "revision": "a27edd78570ec03a301e",
+    "url": "www.privacypal.me/static/js/runtime~main.56ca012f.js"
+  },
+  {
+    "revision": "597c11e3b32b58b9326d",
+    "url": "www.privacypal.me/static/js/main.63030fb9.chunk.js"
+  },
+  {
+    "revision": "e9630f06cae0d7f6bb72",
+    "url": "www.privacypal.me/static/js/2.af2953f7.chunk.js"
+  },
+  {
+    "revision": "597c11e3b32b58b9326d",
+    "url": "www.privacypal.me/static/css/main.44d417e0.chunk.css"
+  },
+  {
+    "revision": "64e447cab9962d8c2a66dcdb19baf655",
+    "url": "www.privacypal.me/index.html"
+  }
+];
