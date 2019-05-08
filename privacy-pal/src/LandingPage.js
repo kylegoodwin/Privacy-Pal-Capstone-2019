b09536f5350bee3fb8747bb4a209d8911c3d6310
swagbot <lucaswoo23@yahoo.com>
2019-05-07 17:53:49 -0700

    adding change from website branch

--- privacy-pal/src/LandingPage.js
+++ privacy-pal/src/LandingPage.js
@@ -37,48 +37,75 @@ class LandingPage extends Component {
         </div>
       {/* EDUCATING THE NEXT GENERATION SECTION */}
       <div>
-        <h1 className="pt-5" id="icons-heading"> Learning Module Topics: </h1>
+        <h1 className="pt-5" id="icons-heading">  Lessons on Social Media: </h1>
       </div>
       <div className = "row">
         {/* <section className="icons" align="center">  */}
-            <div className="column">
+            <div className="col md-4">
               <img className="screen-shots pl-5" src="img/posting-moderation.png"></img>
               <div>
                 <p className="icon-caption text-center font-weight-bold">Posting in Moderation</p>
               </div>
             </div>
-            <div id="2" className="column">
+            <div id="2" className="col md-4">
               <img className="screen-shots" src="img/online-networking.png"></img>
               <div>
                 <p className="icon-caption text-center pr-5 font-weight-bold">Online Friends And Your Network</p>
               </div>
             </div>
-            <div id="3" className="column">
-              <img className="screen-shots pr-4" src="img/economics.png"></img>
+            <div id="3" className="col md-4">
+              <img className="screen-shots pr-4" src="img/identity.png"></img>
               <div>
-                <p className="icon-caption text-center pr-5 font-weight-bold">Economics of Social Media Platforms</p>
+                <p className="icon-caption text-center pr-5 font-weight-bold">Online Identity</p>
               </div>
             </div>
       {/* </section> */}
      </div>
+
+     <div className="examples">
+      <div>
+          <h1 className="pt-5" id="icons-heading">  Learning from Examples: </h1>
+        </div>
+        <div className="container">
+          <div className = "row">
+            {/* <section className="icons" align="center">  */}
+                <div className="col-md-6">
+                  <img className="screen-shots pl-5" src="img/story2.png"></img>
+                  <div>
+                    <p className="icon-caption text-center font-weight-bold">Sensitive Topic Posting</p>
+                  </div>
+                </div>
+                <div id="4" className="col-md-6">
+                  <img className="screen-shots" src="img/story1.png"></img>
+                  <div>
+                    <p className="icon-caption text-center pr-5 font-weight-bold">Unknown Networks</p>
+                  </div>
+                </div>
+          {/* </section> */}
+        </div>
+       </div>
+      </div>
+
      {/* PARALLAX PICTURE SECTION*/}
       <div id="bigImage">
         <div className="text-center">
-        <h1 id="who-we-are">Our Mission</h1>
-        <p className="image-text">Teach</p>
-        <p className="image-text">Protect</p>
-        <p className="image-text">Inspire</p>
+        <h1 id="who-we-are" className="display-1">Our Mission</h1>
+        <p className="image-text display-4">Teach</p>
+        <p className="image-text display-4">Protect</p>
+        <p className="image-text display-4">Inspire</p>
         </div>
       </div>
       {/* MODULES SECTION */}
-      <section className="modules-display">
-        <h1 className="ml-5 text-light"> About the Project </h1>
-        <div className="ml-5 mr-5 text-light">Privacy Pal is a Capstone project through the University of Washington's Informatics program. The team consists of Kyle Goodwin, Lucas Woo,
-          Ryan Peart, and Sam Yoshikawa. Privacy Pal's goal is to inform and educate the next generation on user data, internet privacy, and how to be more 
-          cognizant about their online presence. Our online app is designed for use on smart phones and combines the designs of popular apps in the younger 
-          age groups with educational content and quizzes. We hope to break through to the Middle Schoolers and support them through their digital adventures. 
-        </div>
-      </section>
+      <p id="about">
+        <div className="inner">
+          <h1 className="ml-5 mb-5 display-2"> About the Project </h1>
+          <h5 className="ml-5 mr-5">Privacy Pal is a Capstone project through the University of Washington's Informatics program. The team consists of Kyle Goodwin, Lucas Woo,
+            Ryan Peart, and Sam Yoshikawa. Privacy Pal's goal is to inform and educate the next generation on user data, internet privacy, and how to be more 
+            cognizant about their online presence. Our online app is designed for use on smart phones and combines the designs of popular apps in the younger 
+            age groups with educational content and quizzes. We hope to break through to the Middle Schoolers and support them through their digital adventures. 
+          </h5>
+      </div>
+      </p>
       {/* SIGN UP SECTION */}
       {/* <section className="signUp" align="center">
         <h1> Sign Up Now To Protect Your Online Identity </h1>
