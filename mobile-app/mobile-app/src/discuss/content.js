a3d85a9957062dadfad4f3daca06c5036e2b007f
Kyle Goodwin <kjgoodwins@gmail.com>
2019-05-10 11:22:26 -0700

    css changes to quiz stuff

--- mobile-app/mobile-app/src/discuss/content.js
+++ mobile-app/mobile-app/src/discuss/content.js
@@ -8,6 +8,12 @@ export const Content = [
     
         { type:"response", prompt:"This is a test." },
         { type:"question", question: { prompt: "Who can see your posts?",
+     answers:["Everyone","Just me","My friends","My friends","My friends","My friends","My friends"],
+      correctIndex: 0,
+       correctText: "Nice answer. Always rememeber that people can save and reshare your content!",
+        wrongText: "Nope! Remember, people can reshare your stuff, and its not nessisarily private."
+    }},
+        { type:"question", question: { prompt: "Who can see your posts?",
         answers:["FUCK"],
          correctIndex: 0,
           correctText: "Nice answer. Always rememeber that people can save and reshare your content!",
