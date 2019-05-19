d29d22e0f8f14356e4d967f14951cd638fe5dac1
Sam Yoshikawa <samyosh@uw.edu>
2019-05-19 14:57:36 -0700

    organized story JSON file

--- mobile-app/mobile-app/src/learn/stories.js
+++ mobile-app/mobile-app/src/learn/stories.js
@@ -5,28 +5,79 @@ export let stories = {
               { url: "img/moderation/story2.png", bottomText: "Posting on social media is a great way to let friends catch a glimpse of what is going on in your life" },
               { url: "img/moderation/story3.png", topText: "But posting too frequently can reveal more about your personal life than you want" },
               { url: "img/moderation/story4.png", topText: "If your account is public, you may be posting sensitive information to viewers outside your friend group" },
-              { url: "img/moderation/story5.png", bottomText: "By posting in moderation, you can still keep your followers entertained while keeping your personal life to yourself" },
-              { url: "img/moderation/story6.png", topText: "You've completed this lesson", bottomText: "Tap to the right to head back to the main page" }],
+              { url: "img/moderation/story5.gif", bottomText: "Your posts could also go viral for the wrong reasons" },
+              { url: "img/moderation/story6.png", bottomText: "Some posts could be a crime if someone takes it offensively" },
+              { url: "img/moderation/story7.png", bottomText: "By posting in moderation, you can still keep your followers entertained while keeping your personal life to yourself" },
+              { url: "img/moderation/story8.png", topText: "Story Time", bottomText: "Let’s take a look at a real life problem focused on knowing when to post " },
+              {url:"img/moderation/story9.png",topText:"This is Ben, a 7th grader at a local middle school"},
+              {url:"img/moderation/story10.png",topText:"He is an avid twitter user, star basketball player, and a great student!"},
+              {url:"img/moderation/story11.png",topText:"One day, he sees a tweet about a rumor regarding a female student at his school", bottomText:"The tweet claims the student was cheating on her boyfriend"},
+              {url:"img/moderation/story12.png",topText:"Ben retweeted the tweet and the rumor kept spreading...", bottomText:"As the rumor spread the girl faced bullying and lost some of her friends"},
+              {url:"img/moderation/story13.gif",topText:"What had started as a small tweet quickly spiraled out of Ben’s control"},
+              {url:"img/moderation/story14.png",topText:"His retweet spread to his teachers, parents, and principal"},
+              {url:"img/moderation/story15.png",topText:"Even though he only retweeted once, Ben is held responsible for all the other actions that came from it"},
+              {url:"img/moderation/story16.gif",topText:"Ben was suspended by his school for contributing to bullying"},
+              {url:"img/moderation/story17.png",topText:"For Ben, it was a simple retweet. For the girl it was a nightmare.", bottomText:"Reposting can be easy and quick, but think about the consequences that can happen"},
+              {url:"img/moderation/story18.gif",topText:"Before posting something, or commenting at someone, think to yourself:", bottomText:"“Would I want someone saying this about me?”"},
+              {url:"img/moderation/story19.jpg",topText:"It is important to  not tweet potentially insensitive, harmful, or inappropriate information",bottomText:"Always be careful what you post online. If you have questions ask a parent"},
+              {url:"img/moderation/story19.jpg",topText:"You’re finished with the Posting in Moderation Lesson!",bottomText:"Tap right, or go to the home button to work on the Discussion for this!"}
+       ],
 
        identity: [
-              { url: "img/identity/story13.png", topText: "Your Identity From Your Data", bottomText: "From the things you like, to who you follow, companies know these preferences and use them..." },
-              { url: "img/identity/story14.png", bottomText: "Social media apps like Instagram, FB, and others track your likes, geolocations, what you click and more" },
-              { url: "img/identity/story15.png", bottomText: "All the likes and other information social media gets from you earns them lots of money" },
-              { url: "img/identity/story16.png", bottomText: "Through this information companies know what you like, what you will most likely buy, where you’re from, and can predict how you might behave." },
-              { url: "img/identity/story17.png", topText: "All the data about you is stored by these companies, you don’t know what information they have about you or what they’re doing with it" },
-              { url: "img/identity/story18.png", topText: "You can lose control of not only through social media but other things like games, online quizzes, and other apps." },
-              { url: "https://i.imgur.com/I6kHsll.gif", topText: "You've completed this lesson", bottomText: "Tap to the right to head back to the main page" }
+              { url: "img/identity/story1.png", topText: "Your Identity From Your Data", bottomText: "From the things you like, to who you follow, companies know these preferences and use them..." },
+              { url: "img/identity/story2.png", bottomText: "Social media apps like Instagram, FB, and others track your likes,locations, what you click and more" },
+              { url: "img/identity/story3.png", bottomText: "All the likes and other information social media gets from you earns them lots of money" },
+              { url: "img/identity/story4.png", bottomText: "Through this information companies know what you like, what you will most likely buy, where you’re from, and can predict how you might behave." },
+              { url: "img/identity/story5.png", topText: "All the data about you is stored by these companies", bottomText: "All this data is being sold to advertisement companies to target you" },
+              { url: "img/identity/story6.jpg", topText: "It is important to know that every action you make online shapes who you are" },
+              { url: "img/identity/story7.gif", bottomText: "It is up to you to know when to take action, or not get involved" },
+              { url: "img/identity/story8.jpg", topText: "Story Time", bottomText: "Let’s see a real life situation related to your online identity " },
+              {url:"img/identity/story9.png",topText:"This is Miguel. He is a quiet 7th grader", bottomText: "Miguel likes to go with the flow and have fun doing whatever his friends are doing"},
+              {url:"img/identity/story10.jpg",topText:"One day, his friends add him to a group chat", bottomText: "The group chat is used for mocking and making fun of teachers"},
+              {url:"img/identity/story11.gif",topText:"Miguel realizes the potential trouble he can get in ", bottomText:"The tweet claims the student was cheating on her He quickly leaves the group chat"},
+              {url:"img/identity/story12.png",topText:"One day, one of the members of the group chat was messaging during class", bottomText:"The teacher sees this and takes the phone"},
+              {url:"img/identity/story13.gif",topText:"The teacher sees all the messages and reports the group chat"},
+              {url:"img/identity/story14.gif",topText:"Miguel’s friends are all suspended for 10 days.", bottomText:"Luckily, Miguel was smart to remove himself just in time"},
+              {url:"img/identity/story15.png",topText:"Miguel learned to be careful of the online groups that you are in",bottomText:"Being a part of them can get you in trouble"},
+              {url:"img/identity/story16.gif",topText:"If an online group is hurtful towards someone, don’t get involved",bottomText:"This is cyberbullying and can potentailly get you into trouble and harm others"},
+              {url:"img/identity/story17.gif",topText:"You’re finished with the Online Identity Lesson!", bottomText:"Tap right, or go to the home button to work on the Discussion for this!"},
        ],
 
        friends: [
-              { url: "img/friends/story7.png", topText: "Online Friends & Your Network", bottomText: "How well do you know your ‘friends’ online and why it matters" },
-              { url: "img/friends/story8.png", topText: "Of course your real-life friends are the some of the same ones you have online..." },
-              { url: "img/friends/story9.png", topText: "...but what about the online friends you don’t know at all?" },
-              { url: "img/friends/story10.png", topText: "Anyone can create a profile and make themselves seem like any type of person they want" },
-              { url: "img/friends/story11.png", bottomText: "Anyone can create a profile and make themselves seem like any type of person they want" },
-              { url: "img/friends/story12.png", topText: "You've completed this lesson", bottomText: "Tap to the right to head back to the main page" }
+              { url: "img/friends/story1.png", topText: "Online Friends & Your Network", bottomText: "How well do you know your ‘friends’ online and why it matters" },
+              { url: "img/friends/story2.png", topText: "Some of your friends online are also probably your friends in real life too..." },
+              { url: "img/friends/story3.png", topText: "...but what about the online friends you don’t know at all?" },
+              { url: "img/friends/story4.png", topText: "Anyone can create a profile and make themselves seem like any type of person they want" },
+              { url: "img/friends/story5.png", topText: "Once a friend/follow request is accepted that person can access whatever you’ve put on your social media", bottomText: "You could have no idea who’s behind the account" },
+              { url: "img/friends/story6.gif", topText: "It is important to know who you are talking to online" },
+              { url: "img/friends/story7.gif", topText: "Be sure to look into who they are before interacting with them", bottomText: "Look at their bio, related friends, or recent posts to make sure they are real!" },
+              { url: "img/friends/story8.jpg", topText: "Story Time", bottomText: "Let’s see a real-life example with making friends online" },
+              {url: "img/friends/story9.png", topText: "This is Anna, an 8th grader from your middle school"},
+              {url: "img/friends/story10.png", topText: "She loves to write and is a frequent user of Instagram"},
+              {url: "img/friends/story11.jpg", topText: "Because she has a public account, she constantly has new, random followers", "bottomText": "These followers keep direct messaging her questions"},
+              {url: "img/friends/story12.jpg", topText: "One account pretended to be her friend and asked for sensitive information"},
+              {url: "img/friends/story13.gif", bottomText: "Anna, not knowing any better, sent over her passwords and sensitive information"},
+              {url: "img/friends/story14.jpg", bottomText: "Her personal information, like her home address, was now in the hands of other outsiders"},
+              { url: "img/friends/story15.png", topText: "Later on, her house was broken into", bottomText: "It was by one of the people that asked Anna for her home address"},
+              { url: "img/friends/story16.png", topText: "Anna realized something that day...", bottomText: "It is important to know who you are talking to before revealing yourself"},
+              {url: "img/friends/story17.png", bottomText: "Be sure to look into the account you are talking to before engaging in conversation"},
+              {url: "img/friends/story18.gif", topText: "Make sure that their account does not seem fake or harmful in any way"},
+              {url: "img/friends/story19.gif", bottomText: "By knowing your social media network, you can be safe both online and in real life!"},
+              {url: "img/friends/story20.gif", topText: "You’re finished with the Online Friends and Network Lesson!", "bottomText": "Tap right, or go to the home button to work on the Discussion for this!"},
        ],
 
+       socialMediaIntro: [
+              { url: "img/introduction/story1.png", topText: "Introduction To Social Media", bottomText: "A guide to getting started" },
+              { url: "img/introduction/story2.png", bottomText: "So you want an Instagram (or Snapchat) account?" },
+              { url: "img/introduction/story3.png", topText: "Social media is a great way to stay connected with friends!" },
+              { url: "img/introduction/story4.gif", topText:"Not only can you post stuff about your life", bottomText: "You can also see what your friends have posted!" },
+              { url: "img/introduction/story5.png", bottomText: "But there can be risks involved... " },
+              { url: "img/introduction/story6.jpg", topText: "From scammers, to hackers, and even creeps", bottomText: "Social media has unknown threats lurking in the background" },
+              { url: "img/introduction/story7.png", bottomText: "It is a big responsibility when it comes to having a social media account" },
+              { url: "img/introduction/story8.png", topText: "And we are here to help you become aware of these risks", bottomText: "Tap to the right to get back to the home page and begin learning!" },
+       ],
+
+       /*
        cyberbullying : [
               {url:"img/cyberbullying/background1.png",topText:"This is Ben, a 7th grader at a local middle school"},
               {url:"img/cyberbullying/background2.png",topText:"He is an avid twitter user, star basketball player, and a great student!"},
@@ -45,7 +96,18 @@ export let stories = {
               {url: "img/who/matrixCode.png", topText: "Anna, not knowing any better, sent over her passwords and sensitive information"},
               {url: "img/who/depressedGirl.png", topText: "Anna’s information was then stolen leading to other people having her data", "bottomText": "Anna had to learn the hard way, but you can learn from his mistakes"},
               {url: "img/who/momDaughter.png", topText: "She loves to write and is a frequent user of Instagram"}
-              ]
-
+              ],
+       
+       moderation : [
+              {url:"",topText:"This is Miguel. He is a quiet 7th grader", bottomText: "Miguel likes to go with the flow and have fun doing whatever his friends are doing"},
+              {url:"",topText:"One day, his friends add him to a group chat", bottomText: "The group chat is used for mocking and making fun of teachers"},
+              {url:"",topText:"Miguel realizes the potential trouble he can get in ", bottomText:"The tweet claims the student was cheating on her He quickly leaves the group chat"},
+              {url:"",topText:"One day, one of the members of the group chat was messaging during class", bottomText:"The teacher sees this and takes the phone"},
+              {url:"",topText:"The teacher sees all the messages and reports the group chat"},
+              {url:"",topText:"Miguel’s friends are all suspended for 10 days.", bottomText:"Luckily, Miguel was smart to remove himself just in time"},
+              {url:"",topText:"Miguel learned to be careful of the online groups that you are in",bottomText:"Being a part of them can get you in trouble"},
+              {url:"",topText:"If an online group is hurtful towards someone, don’t get involved",bottomText:"This is cyberbullying and can potentailly get you into trouble and harm others"}
+          ],
+          */
 
 }
